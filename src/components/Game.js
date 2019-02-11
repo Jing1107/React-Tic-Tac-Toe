f7294b3601ceb6dfacc0ecd7fd0863fb0873b886
jing.dong <dj3386083@gmail.com>
2019-02-11 12:02:02 +1100

    first commit

---
+++ src/components/Game.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './App.css'
+import Board from './Board'
+
+class Game extends Component {
+  render() {
+    return (
+      <div className="wrapper">
+        <div className="game">
+          <div className="game-board">
+            <Board />
+          </div>
+          <div className="game-info">
+            <div>{}</div>
+            <div>{}</div>
+          </div>
+        </div>
+    </div>
+    );
+  }
+}
+
+export default Game;
